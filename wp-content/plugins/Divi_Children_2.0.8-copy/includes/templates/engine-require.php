0c00ecafc29469d5b21bca20015ffea616702cc2
alenspider777 <alenspider777@yandex.com>
2017-01-16 18:12:04 +0100

    Init

--- wp-content/plugins/Divi_Children_2.0.8-copy/includes/templates/engine-require.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*****************************************************************************************************************
-Caution: do not remove this or you will lose all the customization capabilities created by Divi Children plugin */
-require_once('divi-children-engine/divi_children_engine.php');
-/****************************************************************************************************************/
-?>
--- wp-content/plugins/Divi_Children_2.0.8-copy/js/tabs-panes.js
+++
@@ -1,9 +0,0 @@
-jQuery.noConflict();
-
-jQuery(document).ready(function($) {
-
-	$( "#dch-tabs" ).tabs();
-	
-});
-
-
